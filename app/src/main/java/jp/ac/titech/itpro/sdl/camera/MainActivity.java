549988a0401c87c460d472ea685492ff899095d5
Advanjef <hino.kent.att@protonmail.com>
2020-05-24 21:00:46 +0900

    Direct JPEG version

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -3,7 +3,9 @@ package jp.ac.titech.itpro.sdl.camera;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +13,19 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import java.io.IOException;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
-    private Bitmap photoImage = null;
+    private Uri photoImage = null;
+    private String currentPhotoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,19 @@ public class MainActivity extends AppCompatActivity {
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        Toast.makeText(MainActivity.this, R.string.cannot_create_file, Toast.LENGTH_LONG).show();
+                    }
+
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        photoImage = photoURI;
+                        startActivityForResult(intent, REQ_PHOTO);
+                    }
                 } else {
                     Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
                 }
@@ -47,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         ImageView photoView = findViewById(R.id.photo_view);
-        photoView.setImageBitmap(photoImage);
+        photoView.setImageURI(photoImage);
     }
 
     @Override
@@ -55,12 +74,23 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                Bundle extra = data.getExtras();
-                if (extra != null) {
-                    photoImage = (Bitmap) extra.get("data");
-                }
+                showPhoto();
             }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
